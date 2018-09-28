220c29092fcffbef606b3d657479044b60442e80
Alejandro Bermudez Holguín <alejo0395@gmail.com>
2018-09-28 15:10:45 -0500

    typo correction in component Board

--- react/game/src/app/screens/Game/components/Board/index.js
+++ react/game/src/app/screens/Game/components/Board/index.js
@@ -12,17 +12,17 @@ class Board extends Component {
     return (
       <React.Fragment>
         <div className={styles.status}>{status}</div>
-        <div className={styles.boardRrow}>
+        <div className={styles.boardRow}>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className={styles.boardRrow}>
+        <div className={styles.boardRow}>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className={styles.boardRrow}>
+        <div className={styles.boardRow}>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
