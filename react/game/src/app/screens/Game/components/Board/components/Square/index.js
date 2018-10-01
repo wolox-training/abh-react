9e82511028819919a7ea99ce7eda76109236e78f
Alejandro Bermudez Holguín <alejo0395@gmail.com>
2018-10-01 17:54:39 -0500

    Advances in game-logic and game design

--- react/game/src/app/screens/Game/components/Board/components/Square/index.js
+++ react/game/src/app/screens/Game/components/Board/components/Square/index.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
-function Square(props) {
-  return (
-    <button className={styles.square} onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
+class Square extends Component {
+  /*
+  * If the game is over, we just render the classic button with the className if its X or O, but none click event
+  */
+
+  /*
+* From here we just render the button as desired, with onClick event, classSelected for its X or O, or null, 
+* and if its a selectable square
+*/
+  state = {
+    hover: false
+  };
+
+  checkClass = () => {
+    if (this.props.value && this.props.value === 'X') {
+      return styles.xSelected;
+    } else if (this.props.value && this.props.value === 'O') {
+      return styles.oSelected;
+    } else if (!this.props.value && !this.props.winner) {
+      return styles.selectableSquare;
+    }
+    return '';
+  };
+
+  checkValue = () => {
+    if (this.props.value) {
+      return this.props.value;
+    }
+    return this.state.hover && !this.props.winner ? this.props.currentPlayer : '';
+  };
+
+  render() {
+    return (
+      <button
+        className={`${styles.square} ${this.checkClass()}`}
+        onClick={this.props.onClick}
+        onMouseEnter={() => this.setState({ hover: true })}
+        onMouseLeave={() => this.setState({ hover: false })}
+      >
+        {this.checkValue()}
+      </button>
+    );
+  }
 }
 
 Square.propTypes = {
   value: PropTypes.string,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.string.isRequired,
+  winner: PropTypes.string
 };
 
 export default Square;
--- react/game/src/app/screens/Game/components/Board/index.js
+++ react/game/src/app/screens/Game/components/Board/index.js
@@ -6,42 +6,69 @@ import styles from './styles.scss';
 class Board extends Component {
   state = {
     squares: Array(9).fill(null),
-    xIsNext: true
+    xIsNext: true,
+    history: [
+      {
+        squares: Array(9).fill(null)
+      }
+    ],
+    stepNumber: 0,
+    winner: null
   };
 
-  playerPlay = i => {
+  handleClick = i => {
     const squares = this.state.squares.slice();
+    if (this.state.winner || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      winner: this.calculateWinner(squares)
     });
   };
 
-  renderSquare = i => <Square onClick={() => this.playerPlay(i)} value={this.state.squares[i]} />;
+  calculateWinner = squares => {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < lines.length; i += 1) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  renderSquare = i => (
+    <Square
+      onClick={() => this.handleClick(i)}
+      value={this.state.squares[i]}
+      currentPlayer={this.state.xIsNext ? 'X' : 'O'}
+      winner={this.state.winner}
+    />
+  );
 
   render() {
-    const status = 'Next player: X';
+    let status;
+    if (this.state.winner) {
+      status = `Winner: ${this.state.winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
-      <Fragment>
-        <div className={styles.status}>{status}</div>
-        <div className={styles.boardRow}>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className={styles.boardRow}>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className={styles.boardRow}>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </Fragment>
+      <div className={styles.boardContainer}>
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+      </div>
     );
   }
 }
