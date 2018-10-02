cf0ddaabc79e1241d0622e97b941d2ad53afee19
Alejandro Bermudez Holguín <alejo0395@gmail.com>
2018-10-02 10:50:11 -0500

    PR last corrections

--- react/game/src/app/screens/Game/index.js
+++ react/game/src/app/screens/Game/index.js
@@ -55,17 +55,17 @@ class Game extends Component {
 
   calculateWinner = squares => {
     const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-    if (squares.every(el => el != null)) {
-      return 'tie';
-    }
     let winner;
+    if (squares.every(el => el != null)) {
+      winner = 'tie';
+    }
     lines.forEach(line => {
       const [a, b, c] = line;
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         winner = squares[a];
       }
     });
-    return winner;
+    return winner || null;
   };
 
   movement = (step, move) => {
