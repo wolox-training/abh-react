812a1281bce29bee7b4eb24df643af45dcf19cbe
Alejandro Bermudez Holguín <alejo0395@gmail.com>
2018-10-10 18:28:36 -0500

    cleaning code

--- react/game/src/config/api.js
+++ react/game/src/config/api.js
@@ -1,6 +1,8 @@
 import { create } from 'apisauce';
 
+const type = 'application/json';
+
 export const API = create({
   baseURL: process.env.REACT_API_BASE_URL,
   timeout: 5000
-});
+}).setHeaders({ Accept: type, 'Content-Type': type });
